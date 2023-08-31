154b92a498eae0c13d02abf35f37d38b5668d22a
Mehmet TAŞ <tasmehmet25@outlook.com>
2023-08-31 05:20:37 +0300

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var cors = require('cors')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
 
 const app = express()
 const port = 3050
@@ -21,6 +21,8 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
+
+const upload = multer({ storage: storage });
 
 
 app.get('/', (req, res) => {
