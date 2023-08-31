67d54dbcdc5b8fcabafe5df144dbd32f6f2ff66d
Mehmet TAŞ <tasmehmet25@outlook.com>
2023-08-31 05:14:02 +0300

    aaaaa

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express()
 const port = 3050
 app.use(cors())
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
 
     res.send("merhaba esma")
@@ -16,6 +20,47 @@ app.get('/', (req, res) => {
 app.post('/test', (req, res) => {
 
    res.send(JSON.stringify(req.body))
+
+})
+
+app.post("/login", (req, res) => {
+
+
+    if(req.body.username == "esma123_11@gmail.com" && req.body.password == "123456789"){
+        res.status(200).send({isLogin:true, message:"Login Başarılı"})
+    }else{
+        res.status(500).json({status:false, message:"Kullanıcı adı veya şifre hatalı"})
+    }
+
+   
+ })
+
+ app.get("/userInfo/:username", (req, res) => {
+
+    if(req.params.username == "esmakoybasi"){
+
+        var data = {
+            username: 'esmakoybasi',
+            bio: 'Biyografi...',
+            avatar: '/img/pp.jpg',
+            pins: [
+              { id: 1, title: '', image: '/img/bg.jpg' },
+              { id: 2, title: '', image: '/img/ee.jpg' },
+              // ...
+            ],
+            savedPosts: [
+              { id: 1, title: 'Kaydedilen Gönderi Başlık 1', image: '/img/m.jpg' },
+              { id: 2, title: 'Kaydedilen Gönderi Başlık 2', image: '/img/m.jpg' },
+              // ...
+            ]
+          }
+    
+            res.status(200).json({message:"Kullanıcı Bilgileri", data:data})
+    }else {
+        res.status(404).json({message:"Kullanıcı Bulunamadı"})
+    }
+
+
 
 })
 
