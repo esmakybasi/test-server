be96ca5c8faf671e056ff62a851f9b64b3231f25
Mehmet TAŞ <tasmehmet25@outlook.com>
2023-08-31 05:20:11 +0300

    aaaa

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+
 app.get('/', (req, res) => {
 
     res.send("merhaba esma")
