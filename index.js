739c9d09f47ecaf05d84c8172e232e5ca3a53cea
esmakybasi <koybasiesma19@gmail.com>
2023-08-31 05:02:41 +0300

    a

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
+var cors = require('cors')
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 const port = 3050
+app.use(cors())
 
 app.get('/', (req, res) => {
+
+    res.send("merhaba esma")
+ 
+ })
+
+app.post('/test', (req, res) => {
 
    res.send(JSON.stringify(req.body))
 
 })
 
-app.post('/', upload.single("profileImage"), (req, res) => {
+app.post('/upload', upload.single("profileImage"), (req, res) => {
 
     //upload file uploads folder
     console.log(req.file)
@@ -23,5 +31,5 @@ app.post('/', upload.single("profileImage"), (req, res) => {
 
 
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:port`)
+console.log(`Example app listening at http://localhost:`+port)
 })
